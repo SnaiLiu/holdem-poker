a9e7abe4bb3183399e58d3573595a39bd77c4dbd
admin <liuzhen@basecity.com>
2017-02-10 00:02:28 +0800

    完成打牌流程中核心函数的实现，并在假定玩家都跟的情况下，实现一个打牌流程。

--- src/holdem_poker/play_proccess.clj
+++ src/holdem_poker/play_proccess.clj
@@ -1,7 +1,83 @@
 (ns holdem-poker.play-proccess
-  "打牌过程")
+  "打牌过程"
+  (:require [holdem-poker.max-cards-type :as mct]
+            [holdem-poker.hand-cards-compare :as hcc]))
+
+(defn next-n-position
+  "相对于当前位置的下n个位置"
+  [position-count curr-position n]
+  (mod (+ curr-position n) position-count))
+
+(defn deal-postions-cards
+  "从第start-index牌开始，将牌发给指定的位置（包括公共牌位置）"
+  [cards start-index positions]
+  (let [cards-count (count positions)
+        card-indexes (range start-index (+ cards-count start-index))]
+    (reduce (fn [r card-index]
+              (let [card (get r card-index)]
+                (->> (assoc card :position (get positions (- card-index start-index)))
+                     (assoc r card-index))))
+            cards card-indexes)))
 
 (defn deal-hole-cards
   "发两张底牌"
-  [cards position-count]
-  nil)
+  [cards position-count button-position]
+  (->> (range 0 (* 2 position-count))
+       (mapv #(next-n-position position-count button-position (+ 2 %)))
+       (deal-postions-cards cards 0)))
+
+(defn deal-n-public-cards
+  "从地start-index牌开始，发n张公共牌"
+  [cards start-index n]
+  (->> (repeat n :public)
+       vec
+       (deal-postions-cards cards start-index)))
+
+(defn position-cards
+  "获得各个位置上的牌（各自底牌+公共牌）"
+  [cards]
+  (let [a-fn (fn [r position-card]
+               (let [{:keys [position card]} position-card]
+                 (if-let [p-cards (get r position)]
+                   (assoc r position (conj p-cards card))
+                   (merge r {position [card]}))))
+        p-cards-map (reduce a-fn {} cards)
+        public-cards (:public p-cards-map)
+        p-cards (dissoc p-cards-map :public)]
+    (mapv (fn [[p cs]] {:position p :cards (concat cs public-cards)}) p-cards)))
+
+(defn max-cards-type
+  "获得各个位置上最大的牌型"
+  [position-cards]
+  (mapv (fn [position-card]
+          (let [cards-type (mct/cards-type (:cards position-card))]
+            {:position (:position position-card)
+             :cards-type cards-type}))
+        position-cards))
+
+(defn winner-positions
+  "获得拥有最大牌型的位置和对应牌型（可能有多个）"
+  [position-cards-types]
+  (hcc/max-hand-cards position-cards-types))
+
+;;=====================================================================
+;; TODO 目前只是假定所有玩家都跟的简易打牌流程，用于测试
+(defn easy-play
+  "简单的打牌流程"
+  [cards position-count button-position]
+  ;; 第一步，各个位置发两张底牌，从大盲位置开始发
+  (-> (deal-hole-cards cards position-count button-position)
+      ;; 第二步，假定都跟，发三张公共牌
+      (deal-n-public-cards (* 2 position-count) 3)
+      ;; 第三步，假定都跟，发第四张公共牌
+      (deal-n-public-cards (+ 3 (* 2 position-count)) 1)
+      ;; 第四步，假定都跟，发第五张公共牌
+      (deal-n-public-cards (+ 4 (* 2 position-count)) 1)
+
+      ;;;;;;;;;; 发牌结束，进行各位置牌型比较
+      ; 各位置拥有的牌（底牌+公共牌）
+      position-cards
+      ; 各位置最大牌型
+      max-cards-type
+      ; 最终赢家
+      winner-positions))
--- test/holdem_poker/play_proccess_test.clj
+++ test/holdem_poker/play_proccess_test.clj
@@ -5,8 +5,8 @@
 
 (defn init-cards
   "初始化一副牌用于测试
-  card-template:  {:owner nil :card [:s :2]}
-  :owner有：某个座位 '0、1、2、3'，公共牌'public'"
+  card-template:  {:position nil :card [:s :2]}
+  :position有：某个座位 '0、1、2、3'，公共牌'public'"
   []
   (let [suits [:s :h :c :d]
         numbers [:2 :3 :4 :5 :6 :7 :8 :9 :t :j :q :k :a]
@@ -14,74 +14,147 @@
                         (map #(do [suit %]) numbers))]
     (->> (map fn-suit-cards suits)
          (apply concat [])
-         (mapv #(do {:owner nil :card %})))))
+         (mapv #(do {:position nil :card %})))))
 
 (deftest init-cards-test
-  (is (= (init-cards) [{:owner nil, :card [:s :2]}
-                       {:owner nil, :card [:s :3]}
-                       {:owner nil, :card [:s :4]}
-                       {:owner nil, :card [:s :5]}
-                       {:owner nil, :card [:s :6]}
-                       {:owner nil, :card [:s :7]}
-                       {:owner nil, :card [:s :8]}
-                       {:owner nil, :card [:s :9]}
-                       {:owner nil, :card [:s :t]}
-                       {:owner nil, :card [:s :j]}
-                       {:owner nil, :card [:s :q]}
-                       {:owner nil, :card [:s :k]}
-                       {:owner nil, :card [:s :a]}
-                       {:owner nil, :card [:h :2]}
-                       {:owner nil, :card [:h :3]}
-                       {:owner nil, :card [:h :4]}
-                       {:owner nil, :card [:h :5]}
-                       {:owner nil, :card [:h :6]}
-                       {:owner nil, :card [:h :7]}
-                       {:owner nil, :card [:h :8]}
-                       {:owner nil, :card [:h :9]}
-                       {:owner nil, :card [:h :t]}
-                       {:owner nil, :card [:h :j]}
-                       {:owner nil, :card [:h :q]}
-                       {:owner nil, :card [:h :k]}
-                       {:owner nil, :card [:h :a]}
-                       {:owner nil, :card [:c :2]}
-                       {:owner nil, :card [:c :3]}
-                       {:owner nil, :card [:c :4]}
-                       {:owner nil, :card [:c :5]}
-                       {:owner nil, :card [:c :6]}
-                       {:owner nil, :card [:c :7]}
-                       {:owner nil, :card [:c :8]}
-                       {:owner nil, :card [:c :9]}
-                       {:owner nil, :card [:c :t]}
-                       {:owner nil, :card [:c :j]}
-                       {:owner nil, :card [:c :q]}
-                       {:owner nil, :card [:c :k]}
-                       {:owner nil, :card [:c :a]}
-                       {:owner nil, :card [:d :2]}
-                       {:owner nil, :card [:d :3]}
-                       {:owner nil, :card [:d :4]}
-                       {:owner nil, :card [:d :5]}
-                       {:owner nil, :card [:d :6]}
-                       {:owner nil, :card [:d :7]}
-                       {:owner nil, :card [:d :8]}
-                       {:owner nil, :card [:d :9]}
-                       {:owner nil, :card [:d :t]}
-                       {:owner nil, :card [:d :j]}
-                       {:owner nil, :card [:d :q]}
-                       {:owner nil, :card [:d :k]}
-                       {:owner nil, :card [:d :a]}])))
+  (is (= (init-cards) [{:position nil, :card [:s :2]}
+                       {:position nil, :card [:s :3]}
+                       {:position nil, :card [:s :4]}
+                       {:position nil, :card [:s :5]}
+                       {:position nil, :card [:s :6]}
+                       {:position nil, :card [:s :7]}
+                       {:position nil, :card [:s :8]}
+                       {:position nil, :card [:s :9]}
+                       {:position nil, :card [:s :t]}
+                       {:position nil, :card [:s :j]}
+                       {:position nil, :card [:s :q]}
+                       {:position nil, :card [:s :k]}
+                       {:position nil, :card [:s :a]}
+                       {:position nil, :card [:h :2]}
+                       {:position nil, :card [:h :3]}
+                       {:position nil, :card [:h :4]}
+                       {:position nil, :card [:h :5]}
+                       {:position nil, :card [:h :6]}
+                       {:position nil, :card [:h :7]}
+                       {:position nil, :card [:h :8]}
+                       {:position nil, :card [:h :9]}
+                       {:position nil, :card [:h :t]}
+                       {:position nil, :card [:h :j]}
+                       {:position nil, :card [:h :q]}
+                       {:position nil, :card [:h :k]}
+                       {:position nil, :card [:h :a]}
+                       {:position nil, :card [:c :2]}
+                       {:position nil, :card [:c :3]}
+                       {:position nil, :card [:c :4]}
+                       {:position nil, :card [:c :5]}
+                       {:position nil, :card [:c :6]}
+                       {:position nil, :card [:c :7]}
+                       {:position nil, :card [:c :8]}
+                       {:position nil, :card [:c :9]}
+                       {:position nil, :card [:c :t]}
+                       {:position nil, :card [:c :j]}
+                       {:position nil, :card [:c :q]}
+                       {:position nil, :card [:c :k]}
+                       {:position nil, :card [:c :a]}
+                       {:position nil, :card [:d :2]}
+                       {:position nil, :card [:d :3]}
+                       {:position nil, :card [:d :4]}
+                       {:position nil, :card [:d :5]}
+                       {:position nil, :card [:d :6]}
+                       {:position nil, :card [:d :7]}
+                       {:position nil, :card [:d :8]}
+                       {:position nil, :card [:d :9]}
+                       {:position nil, :card [:d :t]}
+                       {:position nil, :card [:d :j]}
+                       {:position nil, :card [:d :q]}
+                       {:position nil, :card [:d :k]}
+                       {:position nil, :card [:d :a]}])))
+
+
+(deftest next-n-position-test
+  (are [position-count curr-position n result]
+    (= (next-n-position position-count curr-position n) result)
+
+    2 0 1 1
+    2 0 2 0
+    2 1 3 0
+
+    3 0 1 1
+    3 1 1 2
+    3 1 2 0))
+
+(deftest deal-postions-cards-test
+  (let [initial-cards (init-cards)]
+    (is (= (->> (deal-postions-cards initial-cards 0 [:public :public :public])
+                (take 3))
+           [{:position :public :card [:s :2]}
+            {:position :public :card [:s :3]}
+            {:position :public :card [:s :4]}]
+           ))))
 
 ;; 第一步，每人发牌2张底牌
 (deftest deal-hole-cards-test
   (let [initial-cards (init-cards)]
-    (is (= (deal-hole-cards initial-cards 3)
-           (concat [{:owner "2", :card [:s :2]}
-                    {:owner "0", :card [:s :3]}
-                    {:owner "1", :card [:s :4]}
-                    {:owner "2", :card [:s :5]}
-                    {:owner "0", :card [:s :6]}
-                    {:owner "1", :card [:s :7]}]
-                   (vec (take-last (- 52 6) initial-cards)))))
+    (is (= (deal-hole-cards initial-cards 3 0)
+           (concat [{:position 2 :card [:s :2]}
+                    {:position 0 :card [:s :3]}
+                    {:position 1 :card [:s :4]}
+                    {:position 2 :card [:s :5]}
+                    {:position 0 :card [:s :6]}
+                    {:position 1 :card [:s :7]}]
+                   (vec (take-last (- 52 6) initial-cards)))))))
 
+(deftest deal-n-public-cards-test
+  (let [initial-cards (init-cards)
+        deal-resut-cards (deal-n-public-cards initial-cards 6 3)]
+    (is (= (take 3 (take-last (- 52 6) deal-resut-cards))
+           [{:position :public, :card [:s :8]}
+            {:position :public, :card [:s :9]}
+            {:position :public, :card [:s :t]}]
+           ))))
+
+(deftest position-cards-test
+  (let [        cards [{:position 0, :card [:s :2]}
+                       {:position 1, :card [:s :3]}
+                       {:position 0, :card [:s :4]}
+                       {:position 1, :card [:s :5]}
+                       {:position :public, :card [:s :6]}
+                       {:position :public, :card [:s :7]}
+                       {:position :public, :card [:s :8]}
+                       {:position :public, :card [:s :9]}
+                       {:position :public, :card [:s :t]}]]
+
+    (is (= (position-cards cards)
+           [{:position 0 :cards [[:s :2] [:s :4] [:s :6] [:s :7] [:s :8] [:s :9] [:s :t]]}
+            {:position 1 :cards [[:s :3] [:s :5] [:s :6] [:s :7] [:s :8] [:s :9] [:s :t]]}]))))
+
+(deftest max-cards-type-test
+  (let [position-cards [{:position 0 :cards [[:s :2] [:s :3] [:s :4] [:s :5] [:s :6] [:d :3] [:d :4]]}
+                        {:position 1 :cards [[:c :7] [:d :9] [:s :7] [:h :7] [:d :k] [:d :7] [:d :a]]}]]
+    (is (= (max-cards-type position-cards)
+           [{:position 0 :cards-type [:straight-flush [[:s :6] [:s :5] [:s :4] [:s :3] [:s :2]]]}
+            {:position 1 :cards-type [:4-of-a-kind [[:s :7] [:c :7] [:h :7] [:d :7] [:d :a]]]}]
+           ))
     ))
+
+(deftest winner-positions-test
+  (let [cards [{:position 0 :cards-type [:straight-flush [[:s :6] [:s :5] [:s :4] [:s :3] [:s :2]]]}
+               {:position 1 :cards-type [:4-of-a-kind [[:s :7] [:c :7] [:h :7] [:d :7] [:d :a]]]}]]
+    (is (= (winner-positions cards)
+           [{:position 0 :cards-type [:straight-flush [[:s :6] [:s :5] [:s :4] [:s :3] [:s :2]]]}]))
+    ))
+
+(deftest easy-play-test
+  (let [cards [{:position 0, :card [:s :2]}
+               {:position 1, :card [:h :t]}
+               {:position 0, :card [:s :3]}
+               {:position 1, :card [:h :9]}
+               {:position :public, :card [:s :4]}
+               {:position :public, :card [:s :5]}
+               {:position :public, :card [:s :6]}
+               {:position :public, :card [:h :9]}
+               {:position :public, :card [:h :t]}]]
+    (is (= (easy-play cards 2 0)
+           [{:position 0 :cards-type [:straight-flush [[:s :6] [:s :5] [:s :4] [:s :3] [:s :2]]]}]))))
 
 
